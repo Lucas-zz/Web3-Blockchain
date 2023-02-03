98f86abe4eb811c96fd36cc9716b80c1f5df1253
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-02-03 00:20:48 -0300

    project finish

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+import '@nomiclabs/hardhat-waffle';
+
+export const solidity = '0.8.9';
+export const networks = {
+  goerli: {
+    url: 'https://eth-goerli.g.alchemy.com/v2/CL0myFwYnEvYGE4LQIE1jVENd4nX5Xhp',
+    accounts: ['2dd03e6493a29a0056a826c0ec41c18261a2abc7584406a426d91cbb883860da']
+  }
+};
